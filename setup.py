21ed5e6060ef6d9018955c2d5c18965b6e5d3817
leo6033 <long452a@163.com>
2019-08-10 13:18:02 +0800

    config change

--- setup.py
+++ setup.py
@@ -15,13 +15,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-    'lightgbm>=',
-    'matplotlib>=',
-    'numpy>=',
-    'pandas>=',
-    'scikit-learn>=',
-    'seaborn>=',
-    ],
     packages=find_packages(),
 )
