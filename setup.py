aeec1055270b44f2f879761054b38c919dc50bc4
leo6033 <long452a@163.com>
2019-08-10 13:25:28 +0800

    config change

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import find_packages
 from setuptools import setup
 
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
+
 setup(
     name="handyML ITryagain",
     version="0.0.1",
     author="ITryagain",
     author_email="long452a@163.com",
     description="A library for data science",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/leo6033/handyML",
     classifiers=[
