231b2727d42f4772c397636a6ba7b97d0c5bbcfe
leo6033 <long452a@163.com>
2019-08-13 20:01:00 +0800

    Encoding add class beta_encoding

--- handyML/preprocessing/Encoding.py
+++ handyML/preprocessing/Encoding.py
@@ -310,3 +310,81 @@ def count_encoding(train_data, test_data, columns, is_concat=False, fill_na=True
                     break
     all_encoded = pd.concat(encoded_cols, axis=1)
     return all_encoded.iloc[:train_data.shape[0], :], all_encoded.iloc[train_data.shape[0]:, :]
+
+
+class BetaEncoder(object):
+
+    def __init__(self, group, encoding_nan=False):
+
+        self.group = group
+        self.stats = None
+        self.encoding_nan = encoding_nan
+
+    # get counts from df
+    def fit(self, df, target_col):
+        self.prior_mean = np.mean(df[target_col])
+        tmp = df[[target_col, self.group]].copy()
+        if self.encoding_nan:
+            tmp[self.group] = tmp[self.group].astype(str)
+        stats = tmp[[target_col, self.group]].groupby(self.group)
+        stats = stats.agg(['sum', 'count'])[target_col]
+        stats.rename(columns={'sum': 'n', 'count': 'N'}, inplace=True)
+        stats.reset_index(level=0, inplace=True)
+        self.stats = stats
+
+    # extract posterior statistics
+    def transform(self, df, stat_type, N_min=1):
+        tmp = df[[self.group]].copy()
+        if self.encoding_nan:
+            tmp[self.group] = tmp[self.group].astype(str)
+        df_stats = pd.merge(tmp[[self.group]], self.stats, how='left')
+        n = df_stats['n'].copy()
+        N = df_stats['N'].copy()
+
+        # fill in missing
+        nan_indexs = np.isnan(n)
+        n[nan_indexs] = self.prior_mean
+        N[nan_indexs] = 1.0
+
+        # prior parameters
+        N_prior = np.maximum(N_min - N, 0)
+        alpha_prior = self.prior_mean * N_prior
+        beta_prior = (1 - self.prior_mean) * N_prior
+
+        # posterior parameters
+        alpha = alpha_prior + n
+        beta = beta_prior + N - n
+
+        # calculate statistics
+        if stat_type == 'mean':
+            num = alpha
+            dem = alpha + beta
+
+        elif stat_type == 'mode':
+            num = alpha - 1
+            dem = alpha + beta - 2
+
+        elif stat_type == 'median':
+            num = alpha - 1 / 3
+            dem = alpha + beta - 2 / 3
+
+        elif stat_type == 'var':
+            num = alpha * beta
+            dem = (alpha + beta) ** 2 * (alpha + beta + 1)
+
+        elif stat_type == 'skewness':
+            num = 2 * (beta - alpha) * np.sqrt(alpha + beta + 1)
+            dem = (alpha + beta + 2) * np.sqrt(alpha * beta)
+
+        elif stat_type == 'kurtosis':
+            num = 6 * (alpha - beta) ** 2 * (alpha + beta + 1) - alpha * beta * (alpha + beta + 2)
+            dem = alpha * beta * (alpha + beta + 2) * (alpha + beta + 3)
+
+        else:
+            num = self.prior_mean
+            dem = np.ones_like(N_prior)
+
+        # replace missing
+        value = num / dem
+        value[np.isnan(value)] = np.nanmedian(value)
+        return value
--- handyML/preprocessing/__init__.py
+++ handyML/preprocessing/__init__.py
@@ -1,5 +1,5 @@
 from .Encoding import one_hot_encode, label_encode, freq_encode, mean_encode, \
-    bayesian_target_encoding, nan_encoding, count_encoding
+    bayesian_target_encoding, nan_encoding, count_encoding, BetaEncoder
 
 __all__ = ['one_hot_encode',
            'label_encode',
@@ -7,4 +7,5 @@ __all__ = ['one_hot_encode',
            'mean_encode',
            'bayesian_target_encoding',
            'nan_encoding',
-           'count_encoding']
+           'count_encoding',
+           'BetaEncoder']
